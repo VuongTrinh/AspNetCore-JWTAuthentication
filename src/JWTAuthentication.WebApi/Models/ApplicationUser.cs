14aa39ef284331f18b4ea1d4e0c7410b9c61791f
VuongTrinh <vuongtrinh.dev@gmail.com>
2021-01-27 15:56:40 +0700

    Add refresh token.

--- src/JWTAuthentication.WebApi/Models/ApplicationUser.cs
+++ src/JWTAuthentication.WebApi/Models/ApplicationUser.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using JWTAuthentication.WebApi.Entities;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace JWTAuthentication.WebApi.Models
 {
@@ -6,5 +8,6 @@ namespace JWTAuthentication.WebApi.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public List<RefreshToken> RefreshTokens { get; set; }
     }
 }
--- src/JWTAuthentication.WebApi/Services/IUserService.cs
+++ src/JWTAuthentication.WebApi/Services/IUserService.cs
@@ -8,5 +8,8 @@ namespace JWTAuthentication.WebApi.Services
         Task<string> RegisterAsync(RegisterModel model);
         Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
         Task<string> AddRoleAsync(AddRoleModel model);
+        Task<AuthenticationModel> RefreshTokenAsync(string token);
+        ApplicationUser GetById(string id);
+        bool RevokeToken(string token);
     }
 }
